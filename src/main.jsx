fc79ae02fb400bf74d2c2bbf7b424ed10ea16dd5
Aliaksandr nest <aliaksandrnest@Air-Aliaksandr.bbrouter>
2026-01-19 13:39:03 +0300

    Fix routing for GH pages (basename)

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/coincapwithjs">
     <Provider store={store} >
       <PersistGate loading={null} persistor={persistor}>
       <App />
